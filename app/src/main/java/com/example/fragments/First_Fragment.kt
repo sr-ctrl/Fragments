9d4e7eedf4ef88f3f9b41f7f1f9fabc204a06a5a
sr-ctrl <mr.sfkhan55@gmail.com>
2021-10-26 11:17:39 +0530

    latest commit for fragments communication

--- app/src/main/java/com/example/fragments/First_Fragment.kt
+++ app/src/main/java/com/example/fragments/First_Fragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
+import kotlinx.android.synthetic.main.fragment_first_.*
+import kotlinx.android.synthetic.main.fragment_second.*
 
 class First_Fragment : Fragment() {
 
@@ -14,5 +19,16 @@ class First_Fragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first_, container, false)
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Use the Kotlin extension in the fragment-ktx artifact
+        setFragmentResultListener("requestKey") { requestKey, bundle ->
+            // We use a String here, but any type that can be put in a Bundle is supported
+            val result = bundle.getString("data")
+            // Do something with the result
+            tv_reciever.text = result
+        }
     }
 }
--- app/src/main/java/com/example/fragments/Second_Fragment.kt
+++ app/src/main/java/com/example/fragments/Second_Fragment.kt
@@ -5,6 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
+import kotlinx.android.synthetic.main.fragment_first_.*
+import kotlinx.android.synthetic.main.fragment_second.*
+
 class Second_Fragment : Fragment() {
 
     override fun onCreateView(
@@ -13,5 +19,14 @@ class Second_Fragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
+        btn_sendData.setOnClickListener {
+            val result = Bundle()
+            val input = et_input.text.toString()
+            result.putString("data",input)
+            // Use the Kotlin extension in the fragment-ktx artifact
+            // setFragmentResult("requestKey", bundleOf("bundleKey" to result))
+            setFragmentResult("requestKey",result = result)
+
+    }
     }
 }
